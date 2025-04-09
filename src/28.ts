2db99e8a48bddd1673482187d3e0ba241b042f0d
bomkamplindstrom254 <bomkamplindstrom254@gmail.com>
2025-04-09 04:47:08 +0000

    Add 28.ts

---
+++ src/28.ts
@@ -0,0 +1,8 @@
+// This is an example TypeScript code.
+// It defines two variables: `value` and `message`.
+
+let value = 10;
+let message = "Hello, World!";
+
+console.log(`The value of ` + value + ` is ${value}.`);
+console.log(`The message is: ${message}`);
