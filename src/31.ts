3bab23c7ce429c39455ed1e1e9b4ecc651afca67
bomkamplindstrom254 <bomkamplindstrom254@gmail.com>
2025-04-24 02:54:35 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,13 @@
+// 01. Create a new file named "ts.ts" in the src folder of CodingProject.
+// 02. Write the following code into that file:
+
+// 03. In this code, we have defined a function called "addTwoNumbers".
+
+function addTwoNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+// 04. Call the function and pass it two numbers as arguments.
+let result = addTwoNumbers(2, 3);
+
+console.log(result); // Output: 5
